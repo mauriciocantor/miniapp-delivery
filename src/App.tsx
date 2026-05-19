83cd3680c48e9a9492a70533ab94a145a1c9276d
Mauricio Cantor <nestor.cantorp@claro.com.co>
2026-05-19 11:52:42 -0500

    Cambio titulo de miniprograma

--- src/App.tsx
+++ src/App.tsx
@@ -141,7 +141,7 @@ async function handleGetLocation() {
               Hola, {user?.name.split(' ')[0]} 👋
             </div>
             <div style={{ color: 'white', fontWeight: 700, fontSize: 18 }}>
-              Super Delivery v2
+              Super Delivery Claro
             </div>
           </div>
           {view === 'catalog' && (
